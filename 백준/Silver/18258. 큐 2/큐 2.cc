fa024e9113ddb29afb31dab4ea0a5807dfe05a10
grujam <63233571+grujam@users.noreply.github.com>
2023-05-04 09:08:26 +0900

    [Silver IV] Title: 큐 2, Time: 424 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	queue<int> q;
+
+	for(int i = 0; i < N; i++)
+	{
+		string s;
+		cin >> s;
+
+		if(s == "pop")
+		{
+			if (q.empty())
+				cout << -1 << "\n";
+			else
+			{
+				cout << q.front() << "\n";
+				q.pop();
+			}
+		}
+		else if(s == "size")
+		{
+			cout << q.size() << "\n";
+		}
+		else if(s == "empty")
+		{
+			if (q.empty())
+				cout << 1 << "\n";
+			else
+				cout << 0 << "\n";
+		}
+		else if(s == "front")
+		{
+			if (q.empty())
+				cout << -1 << "\n";
+			else
+				cout << q.front() << "\n";
+		}
+		else if(s == "back")
+		{
+			if (q.empty())
+				cout << -1 << "\n";
+			else
+				cout << q.back() << "\n";
+		}
+		else
+		{
+			int n;
+			cin >> n;
+			q.push(n);
+		}
+	}
+	
+
+
+	return 0;
+}
