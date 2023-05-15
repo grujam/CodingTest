c5577904902fd496d711d7e1ed48d61e9cc41043
grujam <63233571+grujam@users.noreply.github.com>
2023-05-16 01:54:19 +0900

    [Silver III] Title: N과 M (3), Time: 356 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> v;
+
+void PrintVector(vector<int>& v)
+{
+	for(int a : v)
+	{
+		cout << a << " ";
+	}
+	cout << "\n";
+}
+
+void dfs(int idx, int cnt)
+{
+	if (++cnt == M)
+	{
+		PrintVector(v);
+		return;
+	}
+
+	for(int i = 1; i < N+1; i++)
+	{
+		v.push_back(i);
+		dfs(i, cnt);
+		v.pop_back();
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> N >> M;
+
+	for(int i= 1; i < N+1; i++)
+	{
+		v.push_back(i);
+		dfs(i, 0);
+		v.pop_back();
+	}
+
+
+	return 0;
+}
