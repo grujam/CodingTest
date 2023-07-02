0ede586b49c75e295e1e93f227a4b34699aa6401
grujam <63233571+grujam@users.noreply.github.com>
2023-07-03 03:09:12 +0900

    [Silver IV] Title: 덱, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.cc
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+	
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	deque<int> dq;
+
+	string s; int num;
+
+	for(int i = 0; i < N; i++)
+	{
+		cin >> s;
+
+		if(s == "push_back")
+		{
+			cin >> num;
+			dq.push_back(num);
+		}
+		else if(s == "push_front")
+		{
+			cin >> num;
+			dq.push_front(num);
+		}
+		else if(s == "pop_front")
+		{
+			if (dq.empty())
+				cout << -1 << "\n";
+			else
+			{
+				cout << dq.front() << "\n";
+				dq.pop_front();
+			}
+		}
+		else if(s == "pop_back")
+		{
+			if (dq.empty())
+				cout << -1 << "\n";
+			else
+			{
+				cout << dq.back() << "\n";
+				dq.pop_back();
+			}
+		}
+		else if(s == "size")
+		{
+			cout << dq.size() << "\n";
+		}
+		else if(s == "empty")
+		{
+			if (dq.empty())
+				cout << 1 << "\n";
+			else
+				cout << 0 << "\n";
+		}
+		else if(s == "front")
+		{
+			if (dq.empty())
+				cout << -1 << "\n";
+			else
+				cout << dq.front() << "\n";
+		}
+		else if(s == "back")
+		{
+			if (dq.empty())
+				cout << -1 << "\n";
+			else
+				cout << dq.back() << "\n";
+		}
+	}
+	
+	return 0;
+}
