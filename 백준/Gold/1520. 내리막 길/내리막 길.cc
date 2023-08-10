d1463b8bd3463ac5920e135484357802dbb27701
grujam <63233571+grujam@users.noreply.github.com>
2023-08-10 10:54:10 +0900

    [Gold III] Title: 내리막 길, Time: 24 ms, Memory: 4136 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -8,7 +8,7 @@ int N, M;
 int dirx[4] = { 0, -1, 0, 1 };
 int diry[4] = { -1, 0, 1, 0 };
 
-int DFS(int x, int y, vector<vector<int>>& v, vector<vector<bool>>& visited, vector<vector<int>>& path)
+int DFS(int x, int y, vector<vector<int>>& v, vector<vector<int>>& path)
 {
     if (path[x][y] > -1)
         return path[x][y];
@@ -20,16 +20,13 @@ int DFS(int x, int y, vector<vector<int>>& v, vector<vector<bool>>& visited, vec
         int newx = x + dirx[i];
         int newy = y + diry[i];
 
-        if (newx < 0 || newx > N - 1 || newy < 0 || newy > M - 1 || visited[newx][newy] == true)
+        if (newx < 0 || newx > N - 1 || newy < 0 || newy > M - 1)
             continue;
 
         if (v[x][y] <= v[newx][newy] || v[newx][newy] == 0)
             continue;
-
-        visited[newx][newy] = true;
-        path[x][y] += DFS(newx, newy, v, visited, path);
-        visited[newx][newy] = false;
-
+        
+        path[x][y] += DFS(newx, newy, v, path);
     }
 	return path[x][y];
 }
@@ -38,20 +35,17 @@ int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-    
     cin >> N >> M;
 
     vector<vector<int>> v(N, vector<int>(M, 0));
     vector<vector<int>> path(N, vector<int>(M, -1));
-    vector<vector<bool>> visited(N, vector<bool>(M, false));
 
     for (int i = 0; i < N; i++)
         for (int j = 0; j < M; j++)
             cin >> v[i][j];
 
-    visited[0][0] = true;
     path[N - 1][M - 1] = 1;
-    cout << DFS(0, 0, v, visited, path);
+    cout << DFS(0, 0, v, path);
 
     return 0;
 }
