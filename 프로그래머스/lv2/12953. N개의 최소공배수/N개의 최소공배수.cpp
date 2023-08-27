e68188d7e3a470d7900f2b305d740e9ea05f343a
grujam <63233571+grujam@users.noreply.github.com>
2023-08-27 15:20:42 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int solution(vector<int> arr) {
+    int answer = 1;
+    
+    for(const int& a : arr)
+    {
+        answer = lcm(answer, a);
+    }
+    
+    return answer;
+}
